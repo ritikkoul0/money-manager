07a1923dfd57028769c79156e74b06325b333979
ritikkoul <61817094+ritikkoul0@users.noreply.github.com>
2026-04-11 00:23:12 +0530

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,4 +1,19 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
 
 export const config = {
   matcher: [
@@ -10,5 +25,3 @@ export const config = {
     '/transfers/:path*',
   ],
 };
-
-
